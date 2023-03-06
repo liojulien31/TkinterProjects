26c51887e26de28cff224b9872e45018cf9c849c
liojulien31 <43554745+liojulien31@users.noreply.github.com>
2023-03-06 22:46:26 +0100

    Add files via upload
    This is a simple program to generate wifi passwords using tkinter gui that let user enter password length and maximum characters that he want to include.The generated passwords will be saved in a file.Just install requireds librairies for python3 and run your code:
    <b> python3 genpwd.py </b>

---
+++ genpwd.py
@@ -0,0 +1,59 @@
+from tkinter import *
+import random
+
+root = Tk()
+root.geometry("800x200")
+root.title("WPA PASSWORDS GENERATOR")
+#set background with image
+# Add image file
+bg = PhotoImage(file = "./Icons/bulldog.png")
+# Show image using label
+label1 = Label( root, image = bg)
+label1.place(x = 0, y = 0)
+def pwdgen():
+	global E
+	ln = E.get()
+	pwd = ""
+	i = 0
+	while i < int(ln):
+		pwd += random.choice("abX1cX3YdefVghijklWmUnZop0qrs4tuvw2xyzABC5DEF8GHIJKLMNOPQRS9T67!=+*?#/")
+		i += 1
+	return pwd
+
+def pwdformat():
+	root.config(bg="blue")
+	global E2
+	maxc = E2.get()
+	c = 0
+	pwd = ""
+	f = open("pwdgenerated.txt","a")
+	while c < int(maxc):
+		pwd = pwdgen()+ "\n"
+		f.write(pwd)
+		c += 1
+	f.close()
+	E.pack_forget()
+	E2.pack_forget()
+	l.pack_forget()
+	l2.pack_forget()
+	txt = "TASK FINISHED AND "+ maxc + " PASSWORDS ARE GENERATED"
+	l3.config(text=txt)
+	
+
+b = Button(root,text="GENERATEPWD",command=pwdformat)
+b.pack()
+l = Label(root,text="Enter pass length: ",fg="black", font=('Helvetica 20 bold'))
+l.pack()
+E = Entry(root)
+E.pack()
+l2 = Label(root,text="Enter maximum number of pwd: ",fg="black", font=('Helvetica 20 bold'))
+l2.pack()
+E2 = Entry(root)
+E2.pack()
+E.bind("Button-1",pwdformat)
+E2.bind("Button-1",pwdformat)
+l.bind("Button-1",pwdformat)
+l2.bind("Button-1",pwdformat)
+l3 = Label(root,text="",fg="green",font=("Helvetica 20 bold"))
+l3.pack()
+root.mainloop()
